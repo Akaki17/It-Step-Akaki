baf5974fee1387b24dd73ac17fd9c8854598f03f
Akaki17 <166372471+Akaki17@users.noreply.github.com>
2024-11-08 11:02:43 +0400

    Delete myDjango directory

--- myDjango/book_shope/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Category, Product
-
-# Create your views here.
-def index(request):
-    products = Product.objects.all()
-    
-    return render(request, 'index.html', {'products': products})
